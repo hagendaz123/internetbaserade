f33b2328f5a379e4d982fb758c0f4a28e82010ba
Jacob Hagenfeldt <jacob.hagenfeldt.9608@student.uu.se>
2024-01-11 12:41:54 +0100

    Labbkopia

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
-const http = require("http");
-const sh = require("serve-handler");
-const ws = require("ws");
+  const http = require("http");
+  const sh = require("serve-handler");
+  const ws = require("ws");
 
-let count = 0;
-const server = http.createServer((req, res) => {
-  return sh(req, res, { public: "public" });
-});
-
-const wss = new ws.WebSocketServer({ server });
-wss.on("connection", (client) => {
-  console.log("Client connected!");
-  client.on("message", (json) => {
-	console.log("Message: " + json);
-    let msg = JSON.parse(json);
-    if (msg.hasOwnProperty("username")) {
-       client.color = getDarkColor();
-      if (msg.username.trim().length == 0) {
-        client.username = "user_" + ++count; //Tilldelar klienten ett generiskt username (mha en counter) om inget anges
-      } else {
-        client.username = msg.username;
-      }
-      broadcast(
-        `{"user":"${client.username}", "message":" has entered the chat!"}`
-      );
-      
-    } else {
-      if(msg.message.length < 1){
-        alert("NEJNEJNEJ")
-        
-      }
-      else{
-        broadcast(
-          `{"user":"${client.username}", "message":"${msg.message}", "color":"${client.color}"}`
-        );
-      }
-		//Skriv kod som anger vad som skall göras om vi tar emot ett meddelande som INTE innehåller nyckeln/egenskapen "username".  
-    }
+  let count = 0;
+  const server = http.createServer((req, res) => {
+    return sh(req, res, { public: "public" });
   });
-  client.on("close", () => {
-    broadcast(
-      `{"user":"${client.username}", "message":" has left the chat!"}`
+
+  const wss = new ws.WebSocketServer({ server });
+  wss.on("connection", (client) => {
+    console.log("Client connected!");
+    client.on("message", (json) => {
+    //console.log("Message: " + json);
+      let msg = JSON.parse(json); 
+      if (msg.hasOwnProperty("username")) {
+        client.color = getDarkColor();
+        if (msg.username.trim().length == 0) {
+          client.username = "user_" + ++count; //Tilldelar klienten ett generiskt username (mha en counter) om inget anges
+        } else {
+          client.username = msg.username;
+        }
+        broadcast(
+          `{"user":"${client.username}", "message":" has entered the chat!"}`
+        );
+        
+      } else {
+        if(msg.message.length < 1){
+          //if(msg.message){
+          console.log("NEJNEJNEJfrånserver");
+          
+        }
+        else{
+          broadcast(
+            `{"user":"${client.username}", "message":"${msg.message}", "color":"${client.color}", "recipient":"${msg.recipient}", "sender":"${msg.sender}"}`
+          );
+        }
+      //Skriv kod som anger vad som skall göras om vi tar emot ett meddelande som INTE innehåller nyckeln/egenskapen "username".  
+      }
+    });
+    client.on("close", () => {
+      broadcast(
+        `{"user":"${client.username}", "message":" has left the chat!"}`
+      );
+    });
+  });
+
+  function broadcast(msg) {
+    //console.log(msg.sender);
+    for (const client of wss.clients) {
+      if (client.readyState === ws.OPEN /*&& client.username === msg.recipient*/) {
+        client.send(msg);
+      }
+    }
+  }
+  function getDarkColor() {
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += Math.floor(Math.random() * 10);
+    }
+    return color;
+  }
+  // getPubKey = function(username){
+  //   url = 'http://localhost:19608/api/pubkeys/' + encodeURIComponent(username);
+  // 	$.get(url).done(function(data){console.log(data)}).fail(function(error){console.log(errormessage)});
+
+  // }
+  server.listen(process.argv[2] || 8080, () => {
+    console.log(
+      `Server listening on port ${server._connectionKey.split("::::")[1]}...`
     );
   });
-});
-
-function broadcast(msg) {
-  for (const client of wss.clients) {
-    if (client.readyState === ws.OPEN) {
-      client.send(msg);
-    }
-  }
-}
-function getDarkColor() {
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += Math.floor(Math.random() * 10);
-  }
-  return color;
-}
-// getPubKey = function(username){
-//   url = 'http://localhost:19608/api/pubkeys/' + encodeURIComponent(username);
-// 	$.get(url).done(function(data){console.log(data)}).fail(function(error){console.log(errormessage)});
-
-// }
-server.listen(process.argv[2] || 8080, () => {
-  console.log(
-    `Server listening on port ${server._connectionKey.split("::::")[1]}...`
-  );
-});
--- public/js/script.js
+++ public/js/script.js
@@ -7,28 +7,90 @@ ws.onclose = function () {
   console.log("Websocket connection closed");
 };
 ws.onmessage = function (message) {
+
   const msgDiv = document.createElement("div");
   msgDiv.classList.add("msgCtn");
+
   var json = JSON.parse(message.data);
-  console.log(json);
-  msgDiv.innerText = json.user + ": " + json.message;
+  const isOwnMessage = json.sender === username;
+
+  console.log(isOwnMessage);
+
+  if (!json.sender && !json.recipient) {
+    msgDiv.innerText = json.user + ": " + json.message;
+  } else {
+    msgDiv.innerText = isOwnMessage
+      ? `${username}: ${document.getElementById("messageinputBox").value}`
+      : `${json.user}: ${json.recipient === "null" ? json.message : deCryptMessages(json.message)}`;
+  }
+  /*const msgDiv = document.createElement("div");
+  msgDiv.classList.add("msgCtn");
+
+  var json = JSON.parse(message.data);
+   
+  if(json.sender === undefined && json.recipient === undefined){
+    msgDiv.innerText = json.user + ": " + json.message;
+  }
+  else{
+    if(json.sender === username){
+      let ownmessage = document.getElementById("messageinputBox").value;
+      msgDiv.innerText = username + ": " + ownmessage; 
+      
+    }else{
+        //console.log(json);
+      // går in i if:en även om json.recipient är null eller undefined
+      if (json.recipient === "null") {
+        msgDiv.innerText = json.user + ": " + json.message;
+        
+      }else{
+        json.message = deCryptMessages(json.message);
+        msgDiv.innerText = json.user + ": " + json.message; 
+        
+    }
+    }
+  }
+
+  var json = JSON.parse(message.data);
+   
+  if(json.sender !== undefined && json.recipient !== undefined){
+    msgDiv.innerText = json.user + ": " + json.message;
+  }
+  else{
+    if(json.sender === username){
+      let ownmessage = document.getElementById("messageinputBox").value;
+      msgDiv.innerText = username + ": " + ownmessage; 
+      
+    }else{
+        //console.log(json);
+      // går in i if:en även om json.recipient är null eller undefined
+      if (json.recipient === "null") {
+        msgDiv.innerText = json.user + ": " + json.message;
+        
+      }else{
+        json.message = deCryptMessages(json.message);
+        msgDiv.innerText = json.user + ": " + json.message; 
+        
+    }
+    }
+  }*/
   msgDiv.style.backgroundColor = json.color;
- 
-  //Se till att informationen som vi tar emot från servern läggs till i konstanten/diven msgDiv.
   document.getElementById("messages").appendChild(msgDiv);
   chat_div.scrollTop = chat_div.scrollHeight;
-};
+  document.getElementById("messageinputBox").value = "";
+}; 
 
 const user_div = document.getElementById("set_username");
 const chat_div = document.getElementById("chat");
 const userform = document.getElementById("userForm");
 const msgform = document.getElementById("msgForm");
 const errormessage = "User not found";
+let username;
+let ownmessage;
 
 userform.addEventListener('submit',(event) => {
   event.preventDefault();
-  const username = userform.querySelector("#userinputBox").value;
-  const privatekey = userform.querySelector("#privatekeyinputBox").value;
+  //const username = userform.querySelector("#userinputBox").value;
+  username = userform.querySelector("#userinputBox").value;
   getPubKeys();
   let user = new Object();
   user.username = username;
@@ -39,36 +101,97 @@ userform.addEventListener('submit',(event) => {
 
 msgform.addEventListener("submit", (event) => {
   event.preventDefault();
-  const message = document.getElementById("messageinputBox").value;
+  ownmessage = document.getElementById("messageinputBox").value;
   let msg = new Object();
-  msg.message = message;
-  const privatekey = userform.querySelector("#privatekeyinputBox").value; 
-
-  console.log(msg);
-
-  if(msg.message <= 1){
-    alert("NEJNEJNEJ")
-  } else{
-    //crypt(msg);
-    ws.send(JSON.stringify(msg));
+  msg.message = ownmessage;
+  
+  if (msg.message.length < 1) {
+    alert("You cannot send an empty message!");
+  } else {
+    cryptMessage(msg.message)
+      .then((encryptedMessage) => {
+        msg.message = encryptedMessage.msg;
+        msg.recipient = encryptedMessage.recipient;
+        msg.sender = username;
+        ws.send(JSON.stringify(msg));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-  document.getElementById("messageinputBox").value = "";
+  //document.getElementById("messageinputBox").value = "";
 });
+
+
+async function cryptMessage(message){
+  const user = userlist.value;
+  if(user){
+    try {
+      const data = await getPubKey(user);
+      //console.log("Public key retrieved:", data);
+      var crypt = setKey(data);
+      
+      if (crypt) {
+        crypt.msg = crypt.encrypt(message);
+        crypt.recipient = user;
+        return crypt;
+        
+      } else {
+        console.error("Failed to initialize JSEncrypt");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }else{
+    crypt = {msg: message, recipient: null};
+    return crypt;
+  }
+  
+}
+
+function deCryptMessages(encryptedMessage){
+  const privateKey = document.getElementById("privatekeyinputBox").value;
+  //console.log(privateKey);
+  let crypt = new JSEncrypt();
+  crypt.setPrivateKey(privateKey);
+  try {
+    var dec = crypt.decrypt(encryptedMessage);
+    if(dec){
+      return dec;
+    }else{
+      return "Encrypted message";;
+    }
+    //return dec;
+  } catch (error) {
+    alert("cant decrypt");
+  }
+
+}
 msgform.addEventListener("reset", (event) => {
   ws.close();
 });
 
+
 function getPubKeys(){
-  
   url = 'http://localhost:19608/api/pubkeys';
-	$.get(url).done(function(data){displayUsers(data)}).fail(function(error){console.log(error)});
+	$.get(url).done(function(data){displayUsers(data)}).fail(function(error){console.log("The api is not available!")});
 }
+function getPubKey(user) {
+  return new Promise((resolve, reject) => {
+    url = 'http://localhost:19608/api/pubkeys/' + encodeURIComponent(user);
+    $.get(url)
+      .done(data => resolve(data))
+      .fail(error => reject(error));
+  });
+}
+
 function displayUsers(users){
   for (var index = 0; index < users.length; index++) {
-    $('#userlist').append('<option value="' + users[index].username + '">' + users[index].public_key + '</option>');
- }
+    $('#userlist').append('<option value="' + users[index].username + '">' + users[index].username + '</option>');
+ };
 }
-function crypt(){
-  var crypt = new JSEncrypt();
-
+function setKey(key){
+  let crypt = new JSEncrypt();
+  crypt.setPublicKey(key.public_key);  
+  return crypt;
 }
